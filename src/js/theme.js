739f14198fa80581a59f02a6139f1d6261de0dd2
Eliass96 <eliasbg2000@gmail.com>
2025-01-10 08:37:55 +0100

    a ver

--- src/js/theme.js
+++ src/js/theme.js
@@ -9,6 +9,8 @@ document.addEventListener("DOMContentLoaded", () => {
     document.body.classList.add("dark-mode");
     themeToggle.checked = true;
     themeIcon.classList.replace("bi-moon", "bi-sun");
+    iframe.src =
+      "https://cdn.knightlab.com/libs/timeline3/latest/embed/index.html?source=1iDCJ63Psx6COW96lNeMHvx4zEFg3KTkSydP6LEcswvQ&font=Default&lang=en&initial_zoom=2&height=650";
   }
 
   // Cambiar el tema cuando se hace el toggle
@@ -17,10 +19,14 @@ document.addEventListener("DOMContentLoaded", () => {
       document.body.classList.add("dark-mode");
       themeIcon.classList.replace("bi-sun", "bi-moon");
       localStorage.setItem("theme", "dark");
+      iframe.src =
+        "https://cdn.knightlab.com/libs/timeline3/latest/embed/index.html?source=1iDCJ63Psx6COW96lNeMHvx4zEFg3KTkSydP6LEcswvQ&font=Default&lang=en&initial_zoom=2&height=650";
     } else {
       document.body.classList.remove("dark-mode");
       themeIcon.classList.replace("bi-moon", "bi-sun");
       localStorage.setItem("theme", "light");
+      iframe.src =
+        "https://cdn.knightlab.com/libs/timeline3/latest/embed/index.html?source=10Tzlkh2hAS6tt7t8Z_J_E9km9UCMH93X3shjtTelKIw&font=Default&lang=en&initial_zoom=2&height=650";
     }
   });
 });
